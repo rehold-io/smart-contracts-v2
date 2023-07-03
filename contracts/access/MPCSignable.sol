7625d6d9a51b6c0b1e8b740aee288420dac92e28
bifot <michael.semin@icloud.com>
2023-07-03 22:17:44 +0800

    added `ECDSA` lib

--- contracts/access/MPCSignable.sol
+++ contracts/access/MPCSignable.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./MPCManageable.sol";
 
 abstract contract MPCSignable is MPCManageable {
@@ -27,7 +28,7 @@ abstract contract MPCSignable is MPCManageable {
   function _recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) internal pure returns (address) {
     (bytes32 r, bytes32 s, uint8 v) = _splitSignature(_signature);
 
-    return ecrecover(_ethSignedMessageHash, v, r, s);
+    return ECDSA.recover(_ethSignedMessageHash, v, r, s);
   }
 
   function _splitSignature(bytes memory sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
